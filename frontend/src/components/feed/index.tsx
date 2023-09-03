1213494fd423a01b8957082dbbf8467451ffcce9
waihinsoe <waihinsoe332004@gmail.com>
2023-09-03 22:53:50 +0630

    feat: changed ui/ux a little bit

--- frontend/src/components/feed/index.tsx
+++ frontend/src/components/feed/index.tsx
@@ -1,10 +1,33 @@
-import { Box } from "@mui/material";
+import { Box, Button, styled } from "@mui/material";
 import Layout from "../Layout";
+import { useAppDispatch, useAppSelector } from "../../store/hook";
+import { decrement, increment } from "../../store/counterSlice";
 
 const Feed = () => {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+  const BlueButton = styled(Button)({
+    backgroundColor: "skyblue",
+    color: "#fff",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue",
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white",
+    },
+  });
   return (
     <Layout>
-      <Box>This is feed page</Box>
+      <Box sx={{ color: "#ffffff" }}>{count}</Box>
+      <BlueButton onClick={() => dispatch(increment())}>increate</BlueButton>
+      <Button
+        sx={{ color: "#ffff", backgroundColor: "success.main" }}
+        onClick={() => dispatch(decrement())}
+      >
+        decreate
+      </Button>
     </Layout>
   );
 };
--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -9,13 +9,14 @@ import Settings from "../components/settings";
 import Login from "../components/auth/Login";
 import Register from "../components/auth/Register";
 import PrivateRoute from "./PrivateRoute";
+import Feed from "../components/feed";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<PrivateRoute />}>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Feed />} />
           <Route path="/community" element={<Community />} />
           <Route path="/messages" element={<Messages />} />
           <Route path="/notification" element={<Notification />} />
